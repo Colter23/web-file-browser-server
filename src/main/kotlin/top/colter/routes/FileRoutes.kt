eca1a196dc93cfcce9c051e0d4d02e950af55b56
Colter23 <colter_null@163.com>
2024-01-07 16:31:43 +0800

    update: multiple path

--- src/main/kotlin/top/colter/routes/FileRoutes.kt
+++ src/main/kotlin/top/colter/routes/FileRoutes.kt
@@ -6,41 +6,57 @@ import io.ktor.server.routing.*
 import top.colter.models.FileInfo
 import top.colter.models.FolderData
 import top.colter.models.FolderInfo
-import java.io.File
+import java.nio.file.Path
 import kotlin.io.path.*
 
-var basePath = ""
-var baseFile = Path(basePath)
+
+val basePaths = mutableMapOf<String, Path>()
 
 fun Route.fileRouting() {
 
-    basePath = environment?.config?.propertyOrNull("file-browser.path")?.getString() ?: ""
-    baseFile = Path(basePath)
+    val basePathList = environment?.config?.propertyOrNull("file-browser.path")?.getList()
+    basePathList?.forEach {
+        val path = it.replace("\\", "/").removeSuffix("/").split("/").last()
+        basePaths["/$path"] = Path(it)
+    }
 
     route("/file") {
         get("/") {
-            call.respond(getFolderData(""))
+            call.respond(getFolderData())
         }
         get("{path...}") {
-            val path = (call.parameters.getAll("path")?.joinToString("/")) ?: ""
-            val file = baseFile.resolve(path)
+            val pathParams = call.parameters.getAll("path") ?: return@get call.respondText("无法获取path")
+            if (pathParams.isEmpty()) return@get call.respond(getFolderData())
+            val rootPath = "/${pathParams.first()}"
+            val path = if (pathParams.size == 1) "" else {
+                (pathParams.subList(1, pathParams.size).joinToString("/"))
+            }
+            val parentPath = basePaths[rootPath] ?: return@get call.respondText("没有对应路径")
+            val file = parentPath.resolve(path)
             if (!file.isDirectory()) {
                 call.respond(file.readBytes())
             }else {
-                call.respond(getFolderData(path))
+                call.respond(getFolderData(parentPath, path, rootPath))
             }
 
         }
     }
 }
 
-fun getFolderData(path: String): FolderData {
+fun getFolderData(parentPath: Path? = null, path: String = "", rootPath: String = ""): FolderData {
     val folderList = mutableListOf<FolderInfo>()
     val fileList = mutableListOf<FileInfo>()
 
-    val dirs = baseFile.resolve(path).listDirectoryEntries()
-    dirs.forEach {
-        val filePath = it.pathString.replace("\\", "/").removePrefix(basePath)
+    if (rootPath.isEmpty()) {
+        basePaths.forEach { (rootPath, path) ->
+            folderList.add(FolderInfo(rootPath, path.name, path.getLastModifiedTime().toString()))
+        }
+        return FolderData(rootPath, folderList, fileList)
+    }
+
+    val dirs = parentPath?.resolve(path)?.listDirectoryEntries()
+    dirs?.forEach {
+        val filePath = "$rootPath/$path/${it.name}"
         if (it.isDirectory()) {
             folderList.add(FolderInfo(filePath, it.name, it.getLastModifiedTime().toString()))
         }else {
@@ -48,5 +64,5 @@ fun getFolderData(path: String): FolderData {
         }
     }
 
-    return FolderData("/$path", folderList, fileList)
+    return FolderData(rootPath, folderList, fileList)
 }
